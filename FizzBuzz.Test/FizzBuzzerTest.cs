1a119b2dc367b6f05e22cd6258d3afcd7d4d197b
MikolHutnik <mikolhutnik@gmail.com>
2018-05-31 13:01:46 -0400

    class end push

--- FizzBuzz.Test/FizzBuzzerTest.cs
+++ FizzBuzz.Test/FizzBuzzerTest.cs
@@ -17,5 +17,57 @@ namespace FizzBuzz.Test
 
             Assert.That(result, Is.EqualTo("1"));
         }
+
+        [Test]
+        public void Get_Returns_2_for_2()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(2);
+
+            Assert.That(result, Is.EqualTo("2"));
+        }
+
+        [Test]
+        public void Get_Returns_Fizz_for_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+            
+            [Test]
+        public void Get_Returns_Fizz_for_mul_3()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void Get_Returns_4_for_4()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void Get_Returns_Buzz_for_mul_5()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            var result = fizzBuzzer.Get(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+
     }
 }
